a2e6ad933eea3ca6c2119b5f320c843d976e8c07
Joseph Arias <arias.joseph.a@gmail.com>
2018-11-22 09:56:45 -0500

    Integrated new and create actions for Porfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,26 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def new 
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+    if @portfolio_item.valid?
+      @portfolio_item.save
+      redirect_to portfolios_path
+    else  
+      render :new
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
+
+
 end
