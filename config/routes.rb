604de4c0c83b8851e3c1ce6459a55ea365eef990
Joseph Arias <arias.joseph.a@gmail.com>
2018-11-22 16:12:10 -0500

    customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get '/about', to: 'pages#about'
+  resources :portfolios, except: [:show]
+  get '/portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get '/about-me', to: 'pages#about'
   get '/contact', to: 'pages#contact'
   
   resources :blogs
