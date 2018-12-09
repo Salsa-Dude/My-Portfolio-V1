c2e97933e1cc81bbf34c59582cb8001e5a5a0f5b
Joseph Arias <arias.joseph.a@gmail.com>
2018-12-09 12:51:30 -0500

    SetSource to concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
  include DeviseWhitelist
+ include SetSource
 
- before_action :set_source
-
- def set_source 
-  session[:source] = params[:q] if params[:q]
- end
 end
