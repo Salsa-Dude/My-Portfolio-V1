0622eb1a5790088b5a63487247a19c266cebe8fe
Joseph Arias <arias.joseph.a@gmail.com>
2019-02-05 23:25:33 -0500

    Integrad Gravatar image in comment and changed main avatar image

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,4 +1,10 @@
 module BlogsHelper
+
+  def gravatar_helper(user)
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
+  end
+
+
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
       CodeRay.scan(code, language).div
