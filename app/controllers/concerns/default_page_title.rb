b2bbca49d3696d5eac782814a294e151ad5a55b9
Joseph Arias <arias.joseph.a@gmail.com>
2019-10-21 23:09:05 -0400

    added og:image tag

--- app/controllers/concerns/default_page_title.rb
+++ app/controllers/concerns/default_page_title.rb
@@ -8,6 +8,7 @@ module DefaultPageTitle
 
   def set_title 
     @page_title = "My Portfolio | Joseph Arias"
+    @page_preview = "https://www.arias-us.org/wp-content/themes/arias/images/archives/ARIAS_2019_Q3_FINAL-4984.jpg"
   end
 
 end
